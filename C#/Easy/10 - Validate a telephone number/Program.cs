4a14ee7b3405bbf542a73994e391025c4d589b16
Lewis Johnson <lewisj489@gmail.com>
2015-02-28 14:21:47 +0000

    Added 10 and 11

---
+++ C#/Easy/10 - Validate a telephone number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace _10___Validate_a_telephone_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //000 0000 0000
+            var r = new Regex(@"\b\d{3}[-.]?\d{4}[-.]?\d{4}\b");
+            Console.WriteLine("Input home phone number");
+            var n = Console.ReadLine();
+            Console.WriteLine("Validating your number....\n");
+            Console.WriteLine(r.IsMatch(n) ? "You number is a valid UK home number." : "You number is NOT a valid UK home number.");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ C#/Easy/11 - What day is it/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11___What_day_is_it
+{
+    using System.Globalization;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
