d98e6802f743c89ea218043ae4e6b6d36aa450d3
Lewis Johnson <lewisj489@gmail.com>
2015-03-07 11:59:31 +0000

    Pushed some broken stuff

--- C#/Easy/23 - Split me in half/Program.cs
+++ C#/Easy/23 - Split me in half/Program.cs
@@ -6,18 +6,26 @@ using System.Threading.Tasks;
 
 namespace _23___Split_me_in_half
 {
+    using System.Collections;
+
     internal static class Program
     {
         private static void Main(string[] args)
         {
-            List<int> List = new List<int>();
 
             Console.WriteLine("Add some ints with spaces and stuff");
             string[] splitStrings = Console.ReadLine()?.Split(' ');
+            var intArray = Array.ConvertAll(splitStrings, int.Parse);
+            var array1 = intArray.Take(intArray.Length / 2).ToArray();
+            var array2 = intArray.Skip(intArray.Length / 2).ToArray();
 
-            Array.ConvertAll(splitStrings[0], int.Parse());
-            splitStrings.ToList();
+            Console.WriteLine("List 1: {0}\nList 2: {1}", Write(array1), Write(array2));
+            Console.ReadLine();
+        }
+
+        private static string Write(IEnumerable array)
+        {
+            return array.Cast<object>().Aggregate("", (current, item) => current + (item + " "));
         }
     }
-
 }
