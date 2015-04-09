eef45b1835e4857b685a11a15fa97af19c734633
Lewis Johnson <lewisj489@gmail.com>
2015-04-09 13:36:48 +0100

    C# easy 207 & 208

---
+++ C#/Easy/208 - Culling Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _208___Culling_Numbers
+{
+
+    static class Program
+    {
+        static void Main()
+        {
+            var output = new HashSet<int>();
+
+            try
+            {
+                 output = new HashSet<int>(Console.ReadLine().Split(' ').Select(int.Parse));
+            }
+            catch (FormatException)
+            {
+                Console.Clear();
+                Console.WriteLine("Wrong input, try again.");
+                output = new HashSet<int>(Console.ReadLine().Split(' ').Select(int.Parse));
+            }
+
+
+            Console.Write(Environment.NewLine);
+            Console.WriteLine("Result: ");
+            foreach (var item in output)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
