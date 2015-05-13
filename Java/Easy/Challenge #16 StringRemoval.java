25a5723d8a5837130147cbc8deb4852d30499768
Freddie <fv4.programmer@gmail.com>
2015-05-12 20:42:41 -0400

    Add #16 [Easy] Java solution

---
+++ Java/Easy/Challenge #16 StringRemoval.java
@@ -0,0 +1,25 @@
+public static void main(String[] args) {
+                Scanner s = new Scanner(System.in);
+                System.out.println("What string do you want to remove letter from?");
+                String input = s.nextLine();
+                System.out.println("What do you want to remove?");
+                String remove = s.nextLine();
+                System.out.println(letterKiller(input, remove));
+        }
+       
+        private static String letterKiller(String string, String remove){
+                StringBuffer resultString = new StringBuffer("");
+                for(int i=0; i<string.length(); i++){
+                        for (int j= 0; j<remove.length();j++){
+                                if(string.charAt(i)==remove.charAt(j)){
+                                        break;
+                                }else if(j==remove.length()-1){
+                                        resultString.append(string.charAt(i));
+                                }
+                               
+                        }
+                       
+                }
+               
+                return resultString.toString();
+        }
