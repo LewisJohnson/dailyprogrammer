cb46649a7ca7cf914d213e4663a29d6aee709df0
Lewis Johnson <lewisj489@gmail.com>
2015-07-22 01:05:11 +0100

    244 easy c#

---
+++ C#/Easy/224 - Shuffling a List/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _224___Shuffling_a_List
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Shuffle(Console.ReadLine());
+            Console.ReadLine();
+        }
+
+        private static void Shuffle(string shuffle)
+        {
+            var s = shuffle.Split(' ');
+            var r = new Random();
+            var uv = Enumerable.Repeat(-1, s.Length).ToArray();
+            var si = -1;
+            var l = new List<string>();
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                var unused = false;
+                while (!unused)
+                {
+                    si = r.Next(s.Length);
+                    if (!uv.Contains(si))
+                    {
+                        uv[i] = si;
+                        unused = true;
+                    }
+                }
+                l.Add(s[si]);
+            }
+            Console.WriteLine(Environment.NewLine);
+            foreach (var item in l)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+
+}
